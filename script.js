bf7d1bf5ae236d7b54fc85d7771959139cee3c33
Muhammad Talha Jameel <muhammadtalhajameel475@gmail.com>
2025-06-02 23:26:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Sample script for future functionality  
+document.querySelectorAll('.product button').forEach(button => {  
+    button.addEventListener('click', () => {  
+        alert('Item added to cart!');  
+    });  
+});
